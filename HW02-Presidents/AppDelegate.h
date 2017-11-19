d93188b139060b1b8b487fe0e30c20ec6b49b900
Paul Christian <paul.christian001@mymdc.net>
2017-11-19 08:10:55 -0500

    Initial Commit

---
+++ HW02-Presidents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW02-Presidents
+//
+//  Created by PAUL CHRISTIAN on 11/19/17.
+//  Copyright © 2017 PAUL CHRISTIAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW02-Presidents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW02-Presidents
+//
+//  Created by PAUL CHRISTIAN on 11/19/17.
+//  Copyright © 2017 PAUL CHRISTIAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
